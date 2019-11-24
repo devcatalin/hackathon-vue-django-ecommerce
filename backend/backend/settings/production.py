4a0bba426881de14a751d181a6757d7e4bb180c3
Catalin Hoha <catalin.hoha@gmail.com>
2019-11-24 08:10:00 +0200

    updated production settings

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -5,7 +5,7 @@ from dj_database_url import parse as db_url
 from .base import *
 
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = config('SECRET_KEY')
 
@@ -16,14 +16,8 @@ DATABASES = {
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# Whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MIDDLEWARE.insert(  # insert WhiteNoiseMiddleware right after SecurityMiddleware
-    MIDDLEWARE.index('django.middleware.security.SecurityMiddleware') + 1,
-    'whitenoise.middleware.WhiteNoiseMiddleware')
+# # Whitenoise
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# MIDDLEWARE.insert(  # insert WhiteNoiseMiddleware right after SecurityMiddleware
+#     MIDDLEWARE.index('django.middleware.security.SecurityMiddleware') + 1,
+#     'whitenoise.middleware.WhiteNoiseMiddleware')
